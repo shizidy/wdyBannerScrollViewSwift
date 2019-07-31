928601eadc6f80ab6f24ae259c4a8a70b8f29b9b
Macmini <wangdongya1990@163.com>
2019-07-31 10:13:15 +0800

    update

--- wdyBannerScrollViewSwift/ViewController.swift
+++ wdyBannerScrollViewSwift/ViewController.swift
@@ -42,8 +42,6 @@ class ViewController: UIViewController {
             }
             
         }
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
